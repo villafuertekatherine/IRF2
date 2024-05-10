4bc3315caa0ecc13bc13d81ae44e1e7fbb6e5923
villafuertekatherine <72823426+villafuertekatherine@users.noreply.github.com>
2024-05-10 18:44:45 -0400

    IRF 2.0

---
+++ backend/src/main/java/com/pupr/IRF/service/UsersService.java
@@ -0,0 +1,33 @@
+package com.pupr.IRF.service;
+
+import com.pupr.IRF.model.UsersModel;
+import com.pupr.IRF.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public UsersModel registerUser(String login, String password, String email){
+        if (login == null || password == null) {
+            return null;
+        } else {
+            UsersModel usersModel = new UsersModel();
+            usersModel.setLogin(login);
+            usersModel.setPassword(password);
+            usersModel.setEmail(email);
+            return usersRepository.save(usersModel);
+        }
+    }
+
+    public UsersModel authenticate(String login, String password){
+        return usersRepository.findByLoginAndPassword(login, password).orElse(null);
+    }
+}
